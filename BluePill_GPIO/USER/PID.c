3750c48003decaa731d1e41fb05155d6c7fcebbd
QuangNguyenMinh123 <nguyenminhquangcn1@gmail.com>
2023-12-13 23:32:14 +0700

    update

--- BluePill_GPIO/USER/PID.c
+++ BluePill_GPIO/USER/PID.c
@@ -14,23 +14,23 @@
  ******************************************************************************/
 /**************************** DEFINE PID VARIABLE *****************************/
 /* PID Pitch variable */
-static const double PID_Pitch_P_Gain = 1.23;			/* 1.3 */
-static const double PID_Pitch_I_Gain = 0.0075;			/* 0.0112 */
-static const double PID_Pitch_D_Gain = 30;				/* 17.5 */
-static const double PID_Pitch_I_Max  = 400.0;
+static const double PID_Pitch_P_Gain = 1.3;			/* 1.3 */
+static const double PID_Pitch_I_Gain = 0.0112;			/* 0.0112 */
+static const double PID_Pitch_D_Gain = 17.5;				/* 17.5 */
+static const double PID_Max_Pitch  = 400.0;
 
 /* PID Roll variable */
 static const double PID_Roll_P_Gain = PID_Pitch_P_Gain;
 static const double PID_Roll_I_Gain = PID_Pitch_I_Gain;
 static const double PID_Roll_D_Gain = PID_Pitch_D_Gain;
-static const double PID_Roll_I_Max  = PID_Pitch_I_Max;
+static const double PID_Max_Roll  = PID_Max_Pitch;
 
 
 /* PID Yaw variable */
-static const double PID_Yaw_P_Gain 	= 0;		/* 2.0  */
-static const double PID_Yaw_I_Gain 	= 0;		/* 0.015 */
-static const double PID_Yaw_D_Gain 	= 0;		/* 7 */
-static const double PID_Yaw_I_Max  	= 100.0;
+static const double PID_Yaw_P_Gain 	= 4.0;		/* 4.0  */
+static const double PID_Yaw_I_Gain 	= 0.015;		/* 0.015 */
+static const double PID_Yaw_D_Gain 	= 0.0;		/* 0 */
+static const double PID_Max_Yaw  	= 400.0;
 /* Desired angle */
 static double Desired_Pitch;
 static double Desired_Roll;
@@ -56,6 +56,10 @@ static double Pre_Pitch_Error = 0.0;
 static double Pre_Roll_Error = 0.0;
 static double Pre_Yaw_Error = 0.0;
 static double Battery_Compensation = 40.0;
+/* Sum up */
+static double PID_OutputPitch = 0.0;
+static double PID_OutputRoll = 0.0;
+static double PID_OutputYaw = 0.0;
 /********************** END OF DEFINE PID VARIABLE ****************************/
 PID_Data_Type PID_Pwm;
 /*******************************************************************************
@@ -80,9 +84,9 @@ void PID_Calculate(double *PitchVal, double *RollVal, double *YawVal,
 		I_Yaw = 0;
 	}
 	/* Calculating e(t) */
-	Pitch_Error = Desired_Pitch - *PitchVal;
-	Roll_Error  = Desired_Roll  - *RollVal;
-	Yaw_Error   = Desired_Yaw   - *YawVal;
+	Pitch_Error = *PitchVal - Desired_Pitch;
+	Roll_Error  = *RollVal 	- Desired_Roll;
+	Yaw_Error   = *YawVal	- Desired_Yaw;
 	/* Calculate P element */
 	P_Pitch = Pitch_Error * PID_Pitch_P_Gain;
 	P_Roll 	= Roll_Error  * PID_Roll_P_Gain;
@@ -95,35 +99,43 @@ void PID_Calculate(double *PitchVal, double *RollVal, double *YawVal,
 	I_Pitch = I_Pitch + Pitch_Error * PID_Pitch_I_Gain;
 	I_Roll	= I_Roll  + Roll_Error  * PID_Roll_I_Gain;
 	I_Yaw	= I_Yaw   + Yaw_Error   * PID_Yaw_I_Gain;
-	if (I_Pitch > PID_Pitch_I_Max)
-		I_Pitch = PID_Pitch_I_Max;
-	if (I_Roll  > PID_Roll_I_Max)
-		I_Roll  = PID_Roll_I_Max;
-	if (I_Yaw   > PID_Yaw_I_Max)
-		I_Yaw   = PID_Yaw_I_Max;
-	if (I_Pitch < -PID_Pitch_I_Max)
-		I_Pitch = -PID_Pitch_I_Max;
-	if (I_Roll  < -PID_Roll_I_Max)
-		I_Roll  = -PID_Roll_I_Max;
-	if (I_Yaw   < -PID_Yaw_I_Max)
-		I_Yaw   = -PID_Yaw_I_Max;
+	if (I_Pitch > PID_Max_Pitch)
+		I_Pitch = PID_Max_Pitch;
+	if (I_Roll  > PID_Max_Roll)
+		I_Roll  = PID_Max_Roll;
+	if (I_Yaw   > PID_Max_Yaw)
+		I_Yaw   = PID_Max_Yaw;
+	if (I_Pitch < -PID_Max_Pitch)
+		I_Pitch = -PID_Max_Pitch;
+	if (I_Roll  < -PID_Max_Roll)
+		I_Roll  = -PID_Max_Roll;
+	if (I_Yaw   < -PID_Max_Yaw)
+		I_Yaw   = -PID_Max_Yaw;
+	/* PID element sum up */
+	PID_OutputPitch = P_Pitch + I_Pitch + D_Pitch;
+	PID_OutputRoll	= P_Roll + I_Roll + D_Roll;
+	PID_OutputYaw 	= P_Yaw + I_Yaw + D_Yaw;
+	if (PID_OutputPitch > PID_Max_Pitch)
+		PID_OutputPitch = PID_Max_Pitch;
+	else if (PID_OutputPitch < PID_Max_Pitch * -1)	
+		PID_OutputPitch = PID_Max_Pitch * -1;
+	if (PID_OutputRoll > PID_Max_Roll)
+		PID_OutputRoll = PID_Max_Roll;
+	else if (PID_OutputRoll < PID_Max_Roll * -1)	
+		PID_OutputRoll = PID_Max_Roll * -1;
+	if (PID_OutputYaw > PID_Max_Yaw)
+		PID_OutputYaw = PID_Max_Yaw;
+	else if (PID_OutputYaw < PID_Max_Yaw * -1)	
+		PID_OutputYaw = PID_Max_Yaw * -1;
 	/* Sum up */
-	PID_Pwm.FrontRight = GPIO_Pwm->Throttle
-		- (ui16)P_Pitch - (ui16)P_Roll + (ui16)P_Yaw
-		- (ui16)D_Pitch + (ui16)D_Roll 
-		- (ui16)I_Pitch - (ui16)I_Roll + (ui16)I_Yaw;
-	PID_Pwm.FrontLeft  = GPIO_Pwm->Throttle
-		- (ui16)P_Pitch + (ui16)P_Roll - (ui16)P_Yaw
-		- (ui16)D_Pitch - (ui16)D_Roll 
-		- (ui16)I_Pitch + (ui16)I_Roll - (ui16)I_Yaw;
-	PID_Pwm.BackLeft   = GPIO_Pwm->Throttle
-		+ (ui16)P_Pitch + (ui16)P_Roll + (ui16)P_Yaw
-		+ (ui16)D_Pitch - (ui16)D_Roll 
-		+ (ui16)I_Pitch + (ui16)I_Roll + (ui16)I_Yaw;
-	PID_Pwm.BackRight  = GPIO_Pwm->Throttle
-		+ (ui16)P_Pitch - (ui16)P_Roll - (ui16)P_Yaw
-		+ (ui16)D_Pitch + (ui16)D_Roll 
-		+ (ui16)I_Pitch - (ui16)I_Roll - (ui16)I_Yaw;
+	PID_Pwm.FrontRight = GPIO_Pwm->Throttle - (ui16)PID_OutputPitch 
+		+ (ui16)PID_OutputRoll - (ui16)PID_OutputYaw;
+	PID_Pwm.FrontLeft  = GPIO_Pwm->Throttle - (ui16)PID_OutputPitch
+		- (ui16)PID_OutputRoll + (ui16)PID_OutputYaw;
+	PID_Pwm.BackLeft   = GPIO_Pwm->Throttle + (ui16)PID_OutputPitch
+		- (ui16)PID_OutputRoll - (ui16)PID_OutputYaw;
+	PID_Pwm.BackRight  = GPIO_Pwm->Throttle + (ui16)PID_OutputPitch
+		+ (ui16)PID_OutputRoll + (ui16)PID_OutputYaw;
 	/* Battery compensation */
 	PID_Pwm.FrontRight 	+= (ui16) ((12.4 - *BatLevel) * Battery_Compensation);
 	PID_Pwm.FrontLeft 	+= (ui16) ((12.4 - *BatLevel) * Battery_Compensation);
